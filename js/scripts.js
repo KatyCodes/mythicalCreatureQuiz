9407a653a1b5994ee7196abdf86c2e982fc81317
Jena SanCartier and Katy Henning <jena@>
2016-08-11 11:49:41 -0700

    adds to javascript so that only one result shows when toggled

--- js/scripts.js
+++ js/scripts.js
@@ -5,38 +5,65 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
 
-
     var result = $("input:radio[name=radiocolor]:checked").val();
     var result2 = $("input:radio[name=radiofood]:checked").val();
+
     //purple results
     if (result === 'Purple' && result2 === 'Sparkling Water'){
       alert("Your power animal is a Unicorn!")
+        $("#unicorn").show();
+        $("#centaur").hide();
+        $("#phoenix").hide();
     }
     else if (result === 'Purple' && result2 === 'Carrots'){
       alert("Your power animal is a Unicorn")
+      $("#unicorn").show();
+      $("#centaur").hide();
+      $("#phoenix").hide();
     }
     else if (result === 'Purple' && result2 === 'Meat'){
       alert("Your power animal is a Centaur")
+        $("#centaur").show();
+        $("#unicorn").hide();
+        $("#phoenix").hide();
     }
     //red results:
-    if (result === 'Red' && result2 === 'Sparkling Water') {
+    else if (result === 'Red' && result2 === 'Sparkling Water') {
       alert("Your power animal is a Unicorn.")
+      $("#unicorn").show();
+      $("#centaur").hide();
+      $("#phoenix").hide();
     }
     else if (result === 'Red' && result2 === 'Carrots') {
       alert("Your power animal is a Phoenix")
+        $("#phoenix").show();
+        $("#unicorn").hide();
+        $("#centaur").hide();
     }
     else if (result === 'Red' && result2 === 'Meat') {
       alert("Your power animal is a Phoenix")
+      $("#phoenix").show();
+      $("#unicorn").hide();
+      $("#centaur").hide();
     }
     //green results:
-    if (result === 'Green' && result2 === 'Sparkling Water'){
+    else if (result === 'Green' && result2 === 'Sparkling Water'){
       alert("Your power animal is a Centaur")
+      $("#centaur").show();
+      $("#unicorn").hide();
+      $("#phoenix").hide();
     }
     else if (result === 'Green' && result2 === 'Carrots'){
       alert("Your power animal is a Centaur")
+      $("#centaur").show();
+      $("#unicorn").hide();
+      $("#phoenix").hide();
     }
     else if (result === 'Green' && result2 === 'Meat'){
       alert("Your power animal is a Phoenix")
+      $("#phoenix").show();
+      $("#unicorn").hide();
+      $("#centaur").hide();
     }
 
     event.preventDefault();
